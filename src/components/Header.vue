9cfde086fdb380e1aaae7445630375a6b1b96f6d
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-07-24 09:02:13 +0000

    final Project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,78 +1,119 @@
 <template>
-    <section>
-        <nav >
-            <p class="site"><img src="/src/assets/chef-hat (2) 1.png"> Foo</p>
-                <div class="navbar">
-                  <div class="navbars">Home</div>
-                  <div class="navbar">Menu</div>
-                  <div class="navbar">Contact</div>
-                  <div class="navbar">Shop</div>
-    
-                  <div class="search-container">
-                    <input type="text" placeholder="Search...">
-                </div>
-                </div>
-                <HeroSection />
-        </nav>   
-    </section>
-       
-</template>
-<script setup>
-
-</script>
-<style scoped>
-
-.navbar {
-width: 80px;
-height: 58px;
-text-align: right;
-display: flex;
-margin-top: 20px;
-color: #fff;
-margin-left: 15px;
-font-weight: 400;
-font-size: 14px;
-}
-
-.search-container {
-    margin-top: 20px;
+    <header>
+      <nav class="navbar">
+        <div class="site">
+          <img src="/src/assets/chef-hat (2) 1.png" alt="Logo"> Foo
+        </div>
+        <div class="navbar-links">
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">Menu</a>
+          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="#">Shop</a>
+        </div>
+        <div class="search-container">
+          <input type="text" placeholder="Search...">
+        </div>
+      </nav>
+      <HeroSection />
+    </header>
+  </template>
+  
+  <script setup>
+  // Import necessary components or hooks here
+  </script>
+  
+  <style scoped>
+  header {
+    background-color: #333;
+    padding: 10px 20px;
+  }
+  
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  
+  .site {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 700;
+  }
+  
+  .site img {
+    margin-right: 10px;
+  }
+  
+  .navbar-links {
+    display: flex;
+    align-items: center;
+  }
+  
+  .nav-link {
+    color: #fff;
+    text-decoration: none;
+    margin-left: 15px;
+    font-weight: 400;
+    font-size: 14px;
+  }
+  
+  .search-container {
     margin-left: 20px;
-}
-
-.navbars {
-margin-left: 250px;
-margin-top: 20px;
-color: #fff;
-font-weight: 400;
-}
-
-.site {
-color: #fff;
-margin-top: 20px;
-font-size: 20px;
-font-weight: 700;
-float: left;
-}
-
-
-.btn {
-background-color: #D82CD1;
-padding-top: 10px;
-padding-bottom: 10px;
-padding-left: 35px;
-padding-right: 35px;
-border-radius: 6px;
-color: #fff;
-font-size: 13px;
-margin-left: 30px;
-}
-
-.texts {
-    margin-left: 30px;
-}
-
-.page {
-    background-color: #fff;
-}
-
-</style>
+  }
+  
+  .search-container input {
+    padding: 5px;
+    border-radius: 4px;
+    border: none;
+  }
+  
+  @media (max-width: 768px) {
+    .navbar {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+    
+    .site {
+      margin-bottom: 15px;
+    }
+    
+    .navbar-links {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+    
+    .nav-link {
+      margin: 10px 0;
+    }
+  
+    .search-container {
+      width: 100%;
+      margin-top: 10px;
+    }
+  
+    .search-container input {
+      width: 100%;
+      box-sizing: border-box;
+    }
+  }
+  
+  @media (min-width: 769px) and (max-width: 1024px) {
+    .navbar {
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  
+    .navbar-links {
+      flex-direction: row;
+      align-items: center;
+    }
+  
+    .search-container {
+      margin-left: auto;
+    }
+  }
+  </style>
+  
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,136 +1,136 @@
 <template>
-    <section >
-        <img class="img" src="/src/assets/17372 [Converted] 1.png" alt="">
-    <div class="welcome">
-    <h1><span class="fast">Fast</span> <span class="food">Food Delivery</span></h1>
-   </div>
-   <div class="heel">
-    <p>Sed ut perspiciatis
-         unde omnis iste natus sit <br> voluptatem accusantium doloremque laudantium </p>
-    </div>
-    
-    <div>
+    <section class="hero-section">
+      <img class="img" src="/src/assets/17372 [Converted] 1.png" alt="">
+      <div class="welcome">
+        <h1><span class="fast">Fast</span> <span class="food">Food Delivery</span></h1>
+      </div>
+      <div class="heel">
+        <p>Sed ut perspiciatis unde omnis iste natus sit <br> voluptatem accusantium doloremque laudantium</p>
+      </div>
+      <div class="actions">
         <button class="btn">Order Now</button>
-        <a class="watch" href="#">watch video</a>
-    </div>
-
-    <div class="star">
-        <img src="/src/assets/emojione_star.png">
-        <img src="/src/assets/emojione_star.png">
-        <img src="/src/assets/emojione_star.png">
-        <img src="/src/assets/emojione_star.png">
-        <img src="/src/assets/emojione_star.png">
-    </div>
-    <p class="star">5 star rating </p>
-    <p class="stars">based on 1788 reviews</p>
-
+        <a class="watch" href="#">Watch Video</a>
+      </div>
+      <div class="star-rating">
+        <img src="/src/assets/emojione_star.png" alt="Star">
+        <img src="/src/assets/emojione_star.png" alt="Star">
+        <img src="/src/assets/emojione_star.png" alt="Star">
+        <img src="/src/assets/emojione_star.png" alt="Star">
+        <img src="/src/assets/emojione_star.png" alt="Star">
+        <p class="star">5 star rating</p>
+        <p class="stars">based on 1788 reviews</p>
+      </div>
     </section>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-nav {
-    text-align: right;
-    word-spacing: 40px;
-    margin-top: 0;
-    padding-top: 0;
-}
-
-.star {
-    color: black;
-    font-size: 17px;
-    font-weight: 500;
-}
-
-.stars {
- font-size: 17px;
- font-weight: 300;
- color: #fff;
-}
-
-.fast {
- font-family: Poppins;
- font-size: 100px;
- font-weight: 700;
- line-height: 118.5px;
- letter-spacing: -0.035em;
- text-align: left;
- color: #FF7E00;
-}
-
-.food {
-    color: #fff;
-    font-family: Poppins;
-font-size: 70px;
-font-weight: 700;
-line-height: 49.3px;
-text-align: left;
-
-}
-
-
-.watch {
-    margin-left: 20px;
-    color:#fff;
-}
-
-.us {
-    margin-top: 30px;
-
-  .page {
-    background-color: #fff;
-}
-
-}
-.btn {
-    background-color:#0e0d0d;
-    border-radius: 20px;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    padding-left: 25px;
-    padding-right: 25px;
-    color: #fff;
-}
-
-.welcome {
-    text-align: left;
-    margin-top: 80px;
-    margin-left: 25px;
-    width: 820px;
-    height: 160px;
-    top: 276px;
-    left: 297px;
-    font-weight: 800;
-    font-size: 66px;
-    line-height: 80px;
-    letter-spacing: -5%;
-}
-
-.heel {
-    text-align: left;
-    margin-top: 60px;
-    font-family: Poppins;
-    font-size: 19px;
-    font-weight: 400;
-    line-height: 30.54px;
-    text-align: left;
-    color:#909090;
-}
-
-
-.img {
-    width: 440px;
-    height: 400px;
-    top: 1219;
-    float: right;
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .hero-section {
+    text-align: center;
+    padding: 20px;
+    background-color: #fff; /* White background color */
   }
 
-.page {
-    background-color: #fff;
+  
+  .img {
+    width: 100%;
+    max-width: 440px;
+    height: auto;
+    margin: 0 auto;
+    
+    .welcome {
+    margin: 80px 0 25px;
 }
-
-
-
-</style>
+}
+  
+  .fast {
+    font-family: 'Poppins', sans-serif;
+    font-size: 3rem;
+    font-weight: 700;
+    color: #FF7E00;
+  }
+  
+  .food {
+    color: #000;
+    font-family: 'Poppins', sans-serif;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+  
+  .heel {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    color: #909090;
+    margin: 20px 0;
+  }
+  
+  .actions {
+    margin: 20px 0;
+  }
+  
+  .btn {
+    background-color: #0e0d0d;
+    border-radius: 20px;
+    padding: 15px 25px;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+  
+  .watch {
+    margin-left: 20px;
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .star-rating {
+    margin: 20px 0;
+  }
+  
+  .star-rating img {
+    width: 24px;
+    height: 24px;
+  }
+  
+  .star {
+    font-size: 1rem;
+    font-weight: 500;
+    color: #000;
+    margin-top: 10px;
+  }
+  
+  .stars {
+    font-size: 1rem;
+    font-weight: 300;
+    color: #fff;
+    margin-top: 5px;
+  }
+  
+  /*----media query for less than 600 screen size----*/
+  @media only screen and (max-width: 600px) {
+    .welcome h1 {
+      font-size: 1.5rem;
+    }
+  
+    .fast {
+      font-size: 1.5rem;
+    }
+  
+    .food {
+      font-size: 1.2rem;
+    }
+  
+    .heel {
+      font-size: 0.875rem;
+      line-height: 1.5;
+    }
+  
+    .btn {
+      padding: 10px 20px;
+    }
+  }
+  </style>
+  
